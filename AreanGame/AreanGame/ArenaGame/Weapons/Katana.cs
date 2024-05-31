baefba6b706dd2409c07055556b49b8755881400
mimiarabska <mimi_arabska@abv.bg>
2024-05-31 22:54:25 +0300

    update on my files

--- AreanGame/AreanGame/ArenaGame/Weapons/Katana.cs
+++ AreanGame/AreanGame/ArenaGame/Weapons/Katana.cs
@@ -22,20 +22,18 @@ namespace ArenaGame.Weapons
             return random.NextDouble() <= CriticalHitChance / 100.0;
         }
 
-
         public Katana(string name)
         {
             Name = name;
             AttackDamage = 40;
+            if (IsCriticalHit)
+            {
+                AttackDamage += AttackDamage*2;
+            }
             BlockingPower = 15;
             Range = 2;
             CriticalHitChance = 15;
         }
-        public void Attack()
-        {
-            bool isCriticalHit = IsCriticalHit();
-            double finalDamage = isCriticalHit ? AttackDamage * 2 : AttackDamage;
-            Console.WriteLine($"{Name} attack: {finalDamage} damage{(isCriticalHit ? " (Critical Hit!)" : "")}");
-        }
+
     }
 }
